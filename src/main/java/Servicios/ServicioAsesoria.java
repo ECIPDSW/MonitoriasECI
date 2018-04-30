2967cb0683b2052d301a801ef495af1a974543e3
Nicolas <nicoga97@gmail.com>
2018-04-30 15:32:23 -0500

    creacion requisitos para consultas en la base de datos

--- src/main/java/Servicios/ServicioAsesoria.java
+++ src/main/java/Servicios/ServicioAsesoria.java
@@ -5,6 +5,9 @@
  */
 package Servicios;
 import Modelo.*;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author David Rodriguez
@@ -13,5 +16,139 @@ public interface ServicioAsesoria {
     Semestre getSemestre(int id);
     Profesor getProfesor(int id);
     Monitor loadMonitor(int id);
-    void registrarAsesoria(int grupo, int monitor, int idEstudiante,String nombreEstudiante, String observaciones);
+    void registrarAsesoria(int grupo, int monitoriaRegistrada, int idEstudiante, String observaciones);
+    /**
+     * registra el inicio de una monitoria en especifico por un monitor
+     * @param idMonitoria
+     * @param ip 
+     */
+    void registrarInicioMonitoriaDictada(int idMonitoria,String ip);
+    
+    /**
+     * registra el fin de la franja de un monitor
+     * @param idMonitoria
+     * @param ip 
+     */
+    void registrarFinMonitoria(int idMonitor);
+    
+    /**
+     * devuelve todas las monitorias que estan disponibles para dictar por el monitor 
+     * en la hora actual
+     * @param idMonitor
+     * @return 
+     */
+    List<Monitoria> monitoriasDisponiblesParaDictar(int idMonitor);
+    /**
+     * Carga todos los cursos disponibles  
+     * @return 
+     */
+    List<Curso> loadCursos();
+    /**
+     * Retorna todas las monitorias dictadas en un semestre especifico
+     * @return 
+     */
+    List<Monitoria> loadMonitorias(int semestre);
+  
+     /**
+     * Retorna todas las monitorias disponibles en el semestre actual de un curso en especifico
+     * @return 
+     */
+    List<Monitoria> loadMonitorias(String idcurso, int semestre);
+    
+     /**
+     * Retorna todas las asesorias dictadas  el semestre especificado
+     * @return 
+     */
+    List<Asesoria> loadAsesorias(int semestre);
+    /**
+     * Retorna todas las asesorias dictadas  por tema especificado
+     * @return 
+     */
+    List<Asesoria> loadAsesoriasPorTema(int idTema,int semestre);
+    /**
+     * Retorna todas las asesorias dictadas  por monitor
+     * @return 
+     */
+    List<Asesoria> loadAsesoriasPorMonitor(int idMonitor,int semestre);
+    
+    /**
+     * Retorna todas las asesorias dictadas  por Grupo
+     * @return 
+     */
+    List<Asesoria> loadAsesoriasPorGrupo(int idGrupo);
+      /**
+     * Retorna todas las asesorias dictadas  por curso
+     * @return 
+     */
+    List<Asesoria> loadAsesoriasPorCurso(String idCurso);
+    
+    /**
+     * retorna todos los semestres que han sido creados
+     * @return 
+     */
+    List<Semestre> loadSemestres();
+    /**
+     * retorna el semestre actual
+     * @return 
+     */
+    Semestre loadSemestreActual();
+    
+    /**
+     * retorna los temas disponibles para un curso
+     * @param idCurso
+     * @return 
+     */
+    List<TemaCurso> loadTemasCurso(int idCurso);
+    
+      /**
+     * retorna los temas dicrtados en una asesoria
+     * @param idCurso
+     * @return 
+     */
+    List<TemaAsesoria> loadTemasAsesoria(int idAsesoria);
+    
+    /**
+     * devuelve todos los monitores por profesor
+     * @param idProfesor
+     * @return 
+     */
+    List<Monitor> LoadMonitoresAsociadosProfesor(int idProfesor,int semestre);
+    
+      /**
+     * devuelve todos los grupos por profesor
+     * @param idProfesor
+     * @return 
+     */
+    List<Grupo> LoadGruposAsociadosProfesor(int idProfesor,int semestre);
+    
+    /**
+     * retorna el administrador con las credenciales registradas
+     * @param id
+     * @param psw
+     * @return 
+     */
+    Administrador loadAdmin(int id,String psw);
+    
+    /**
+     * retorna el monitor con las credenciales registradas
+     * @param id
+     * @param psw
+     * @return 
+     */
+    Monitor loadMonitor(int id,String psw);
+    
+    /**
+     * retorna el Profesor con las credenciales registradas
+     * @param id
+     * @param psw
+     * @return 
+     */
+    Profesor loadProfesor(int id,String psw);
+    
+    /**
+     * retorna todods los grupos existentes para un curso
+     * @param idCurso
+     * @return 
+     */
+    List<Grupo> loadGrupos(String idCurso,int semestre);
 }
--- src/main/java/ServiciosImpl/ServicioAsesoriaImpl.java
+++ src/main/java/ServiciosImpl/ServicioAsesoriaImpl.java
@@ -12,15 +12,23 @@ import DAOS.MonitoriaRegistradaDAO;
 import DAOS.ProfesorDAO;
 import DAOS.SemestreDAO;
 import DAOS.TemaAsesoriaDAO;
+import Modelo.Administrador;
+import Modelo.Asesoria;
+import Modelo.Curso;
+import Modelo.Grupo;
 import Modelo.Monitor;
+import Modelo.Monitoria;
 import Modelo.Profesor;
 import Modelo.Semestre;
+import Modelo.TemaAsesoria;
+import Modelo.TemaCurso;
 import MyBatis.MyBatisMonitorDao;
 import MyBatis.MyBatisProfesorDao;
 import MyBatis.MyBatisSemestreDao;
 import Servicios.ServicioAsesoria;
 import com.google.inject.Inject;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  *
@@ -61,9 +69,116 @@ public class ServicioAsesoriaImpl implements ServicioAsesoria,Serializable{
         return monitor.loadMonitor(id);
     }
 
+    
+
     @Override
-    public void registrarAsesoria(int grupo, int monitor, int idEstudiante, String nombreEstudiante, String observaciones) {
-        asesoria.registrarAsesoria(grupo, monitor, idEstudiante, nombreEstudiante, observaciones);
+    public void registrarAsesoria(int grupo, int monitoriaRegistrada, int idEstudiante, String observaciones) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void registrarInicioMonitoriaDictada(int idMonitoria, String ip) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void registrarFinMonitoria(int idMonitor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Monitoria> monitoriasDisponiblesParaDictar(int idMonitor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Curso> loadCursos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Monitoria> loadMonitorias(int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Monitoria> loadMonitorias(String idcurso, int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Asesoria> loadAsesorias(int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Asesoria> loadAsesoriasPorTema(int idTema, int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Asesoria> loadAsesoriasPorMonitor(int idMonitor, int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Asesoria> loadAsesoriasPorGrupo(int idGrupo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Asesoria> loadAsesoriasPorCurso(String idCurso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Semestre> loadSemestres() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Semestre loadSemestreActual() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<TemaCurso> loadTemasCurso(int idCurso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<TemaAsesoria> loadTemasAsesoria(int idAsesoria) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Monitor> LoadMonitoresAsociadosProfesor(int idProfesor, int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Grupo> LoadGruposAsociadosProfesor(int idProfesor, int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Administrador loadAdmin(int id, String psw) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Monitor loadMonitor(int id, String psw) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Profesor loadProfesor(int id, String psw) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Grupo> loadGrupos(String idCurso, int semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
    
     
