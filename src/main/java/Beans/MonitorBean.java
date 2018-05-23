3ba572931e9133a74fd152184b245cdfa328658c
Oscar Pinto <moka117@hotmail.com>
2018-05-23 13:17:27 -0500

    Registrarasesoria casi finalizado

--- src/main/java/Beans/MonitorBean.java
+++ src/main/java/Beans/MonitorBean.java
@@ -1,11 +1,16 @@
 package Beans;
 
+import Modelo.Asesoria;
+import Modelo.Curso;
 import Modelo.Grupo;
 import Modelo.Monitor;
 import Modelo.Monitoria;
+import Modelo.MonitoriaRegistrada;
 import Modelo.Semestre;
+import Modelo.TemaCurso;
 import Servicios.Fabrica;
 import Servicios.ServicioAsesoria;
+import java.io.IOException;
 import java.io.Serializable;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -14,6 +19,7 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.lang.String;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -35,44 +41,58 @@ public class MonitorBean implements Serializable {
     private Monitor monitor;
     private Monitoria monitoriaEnCurso;
     private boolean monitoriaIniciada = false;
-    private ArrayList<Grupo> grupos;
-    private Grupo grupoSeleccionado;
+    
 
     private String horaInicioMonitoria;
     private String horaFinMonitoria;
 
-    private String car;
-    private List<SelectItem> cars;
-
-    private String city;
-    private Map<String, String> cities = new HashMap<String, String>();
-
+  
+    private String estudiante;
     private String profesor;
     private List<String> profesores;
 
     private String curso;
     private List<String> cursos;
-
+    
+    private Map<String,Grupo> grupos = new HashMap<>();
+    private Map<String,String>gruposs = new HashMap<>();
+    private Grupo grupo;
+    private String gruponame="";
+    
+    private Map<String,TemaCurso> temas = new HashMap<>();
+    private List<String> temass = new ArrayList<>();
+    private TemaCurso tema;
+    private String temaname="";
+    private List<TemaCurso> selectedTemas = new ArrayList<>();
+    private String[] selectedTemase= new String[100];
+    private List<Asesoria> aencurso= new ArrayList<>();
     public void MonitorBean() {
 
     }
 
-    public ArrayList<Grupo> getGrupos() {
-        return grupos;
+ 
+    public Map<String, String> getGrupos() {
+        //System.out.println("LLAMADOG");
+        return gruposs;
+    }
+    public void setGrupo(String name){
+        //System.out.println("LLAMADOGS");
+        this.gruponame=name;
+        grupo=grupos.get(name);
+    }
+    public String getGrupo(){
+        return gruponame;
     }
 
-    public void setGrupos(ArrayList<Grupo> grupos) {
-        this.grupos = grupos;
+ 
+    public String getGrupoName(){
+        return gruponame;
     }
-
-    public Grupo getGrupoSeleccionado() {
-
-        return grupoSeleccionado;
+    public void setGrupoName(String name){
+            grupo=grupos.get(name);
+        this.gruponame=name;
     }
-
-    public void setGrupoSeleccionado(Grupo grupoSeleccionado) {
-        this.grupoSeleccionado = grupoSeleccionado;
-    }
+    
 
     public String getMonitoriaIniciada() {
 
@@ -126,6 +146,9 @@ public class MonitorBean implements Serializable {
         if (monitoriaEnCurso != null) {
             monitoriaIniciada = true;
             sa.registrarInicioMonitoriaDictada(monitoriaEnCurso.getIdMonitoria(), ipAddress);
+            Gruposmonitoriaactual();
+            TemasCursoactual();
+            Asesoriasactuales();
         } else {
             FacesContext context = FacesContext.getCurrentInstance();
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"No esta en su franja de monitoria", "Actualmente usted no tiene registrada ninguna monitoria para dictar."));
@@ -134,11 +157,95 @@ public class MonitorBean implements Serializable {
 
     public void botonFinalizarAsesoria() {
         sa.registrarFinMonitoria(monitor.getId());
+        monitoriaEnCurso=null;
+        monitoriaIniciada=false;
     }
 
     public void setMonitor(Monitor m) {
         this.monitor = m;
+    }
+    
+    public void Asesoriasactuales(){
+        List<MonitoriaRegistrada> mr=sa.loadMonitoriasRegistradasPorMonitoria(monitoriaEnCurso.getIdMonitoria());
+        MonitoriaRegistrada mre = mr.get(mr.size()-1);
+        aencurso=sa.loadAsesoriasRealizandosePorMonitoriaRegistrada(mre.getIdMonitoria());
+    
+    }
+    public void monitoring() throws IOException{
+        if(monitoriaEnCurso== null){
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"No esta en su franja de monitoria", "Actualmente usted no tiene registrada ninguna monitoria para dictar."));
+ 
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/monitorias/InicioMonitoria.xhtml");
+            
+        }
+    }
+    public void redirected(){
+        if(monitoriaEnCurso== null){
+        FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Usted no ha iniciado su monitoria", "Actualmente usted no tiene iniciada ninguna monitoria para dictar."));
+        }
+    }
+    public void Gruposmonitoriaactual(){
+        Curso c=sa.loadCursoPorMonitoria(monitoriaEnCurso.getIdMonitoria());
+        List<Grupo> gr =sa.loadGrupos(c.getId(), sa.loadSemestreActual().getNumero());
+
+        for(int i=0;i<gr.size();i++){
+            grupos.put(gr.get(i).getCurso().getNombre()+""+gr.get(i).getNumero(),gr.get(i));
+            gruposs.put(gr.get(i).getCurso().getNombre()+""+gr.get(i).getNumero(),gr.get(i).getCurso().getNombre()+""+gr.get(i).getNumero());
+        }
 
     }
+    public void TemasCursoactual(){
+        Curso c=sa.loadCursoPorMonitoria(monitoriaEnCurso.getIdMonitoria());
+        List<TemaCurso> tem=sa.loadTemasCurso(c.getId());
+        System.out.println("TEMAAAS"+tem.size());
+        for(int i=0;i<tem.size();i++){
+            temass.add(tem.get(i).getTema());
+            temas.put(tem.get(i).getTema(), tem.get(i));
+        }
+    }
+    public List<String> getTemas() {
+        return temass;
+    }
+    
+    public void setSelectedTemas(String[] selectedTemass) {
+        selectedTemase=selectedTemass;
+        for (String a:selectedTemass){
+            selectedTemas.add(temas.get(a));
+        }
+    }
+    public String[] getSelectedTemas() {
+        return selectedTemase;
+    }
+    
+    public void botonRegistrarAsesoria() {
+        List<MonitoriaRegistrada> mr=sa.loadMonitoriasRegistradasPorMonitoria(monitoriaEnCurso.getIdMonitoria());
+        MonitoriaRegistrada mre = mr.get(mr.size()-1);
+        sa.registrarAsesoria(grupo.getIdGrupo(),mre.getIdMonitoria(), Integer.parseInt(estudiante));
+        estudiante="";
+        grupo=null;
+    }
 
+    public String getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(String estudiante) {
+        this.estudiante = estudiante;
+        
+    }
+
+    public List<Asesoria> getAencurso() {
+        List<MonitoriaRegistrada> mr=sa.loadMonitoriasRegistradasPorMonitoria(monitoriaEnCurso.getIdMonitoria());
+        MonitoriaRegistrada mre = mr.get(mr.size()-1);
+        aencurso=sa.loadAsesoriasRealizandosePorMonitoriaRegistrada(mre.getIdMonitoria());
+        return aencurso;
+    }
+
+    public void setAencurso(List<Asesoria> aencurso) {
+        this.aencurso = aencurso;
+    }
+    
+    
 }
